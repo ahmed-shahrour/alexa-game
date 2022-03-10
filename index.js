a2a79a8243b398b3bf822b62e42f0b28ff4ee5cd
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-03-10 08:50:47 -0800

    ErrorHandler dummy

--- index.js
+++ index.js
@@ -46,3 +46,18 @@ const SessionEndedRequestHandler = {
   },
 };
 
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
