f3489d991b641c690bc82e5c43738fe07b22031a
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-03-10 08:50:10 -0800

    LaunchRequestHandler dummy

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const Alexa = require('ask-sdk-core');
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = 'Welcome to the Alexa Skills Kit, you can say hello!';
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
