c6ae32f4c571d40362cc3194be8b175198aeec74
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-03-14 21:04:26 -0700

    Extra bug fix

--- src/utils/getExpectedResponse.js
+++
@@ -1,8 +0,0 @@
-const getExpectedResponse = (counter) =>{
-  if (counter % 3 === 0 && counter % 5 === 0) return 'fizz buzz';
-  else if (counter % 3 === 0) return 'fizz';
-  else if (counter % 5 === 0) return 'buzz';
-  else return counter;
-}
-
-module.exports = getExpectedResponse;
