86144608d22551c4089276f3b0c77efe62365d5c
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-03-14 19:54:03 -0700

    StartGameIntent

--- index.js
+++ index.js
@@ -17,6 +17,27 @@ const LaunchRequestHandler = {
   },
 };
 
+const StartGameIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'StartGameIntent'
+    );
+  },
+  handle(handlerInput) {
+    const sessionAttributes =
+      handlerInput.attributesManager.getSessionAttributes();
+    sessionAttributes.counter = 1;
+    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+    const speechText = `OK, I'll start... ${sessionAttributes.counter}.`;
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
