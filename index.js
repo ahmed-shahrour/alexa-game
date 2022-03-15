fdccc2747f464aa04629c3ef681522ee7cb5a322
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-03-14 19:57:56 -0700

    RepeatIntent & HelpIntent

--- index.js
+++ index.js
@@ -85,6 +85,18 @@ const InProgressGameIntentHandler = {
       .getResponse();
   },
 };
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent'
+    );
+  },
+  handle(handlerInput) {
+    const speechText =
+      "Choose one of the following options: \n 1. RESTART: Say RESTART to start the game over. \n 2. STOP: Say STOP to end the game. \n 3. REPEAT: Say REPEAT to repeat the last word I said. \n 4. INSTRUCTIONS: Say INSTRUCTIONS to hear the game's instructions again. \n 4. HELP: Say HELP to hear this message again.";
+
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
@@ -103,22 +115,39 @@ const CancelAndStopIntentHandler = {
     );
   },
   handle(handlerInput) {
-    const speakOutput = 'Goodbye!';
+    const speakOutput = 'Thanks for playing, that was fun! Goodbye!';
 
-    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .withShouldEndSession(true)
+      .getResponse();
   },
 };
 
-const SessionEndedRequestHandler = {
+const RepeatIntentHandler = {
   canHandle(handlerInput) {
     return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) ===
-      'SessionEndedRequest'
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'RepeatIntent'
     );
   },
   handle(handlerInput) {
-    // Any clean-up logic goes here.
-    return handlerInput.responseBuilder.getResponse();
+    const sessionAttributes =
+      handlerInput.attributesManager.getSessionAttributes();
+
+    if (!sessionAttributes.counter) {
+      const speechText = "I didn't start yet. Say START to begin.";
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    }
+    return handlerInput.responseBuilder
+      .speak(`${getExpectedResponse(sessionAttributes.counter)}`)
+      .reprompt(`${getExpectedResponse(sessionAttributes.counter)}`)
+      .getResponse();
+  },
+};
 
 const InstructionsIntentHandler = {
   canHandle(handlerInput) {
